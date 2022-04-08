e38826eff77a7b66d8c21efc908103d0ad9bc0f2
Milo Fisher <tbfmilo@gmail.com>
2022-04-07 20:24:22 -0700

    Added example file in character creation

---
+++ Assets/Scripts/Manager Scripts/CharacterCreationManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AdventuresOfOld;
+using Unity.Netcode;
+
+public class CharacterCreationManager : MonoBehaviour
+{
+    Player localPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        // Gets local player
+        foreach(GameObject p in players)
+        {
+            if (p.GetComponent<Player>().IsLocalPlayer)
+            {
+                localPlayer = p.GetComponent<Player>();
+            }
+        }
+
+        // Set Variable
+        localPlayer.SetValue("Name", "Bob");
+        localPlayer.SetValue("Gold", 100);
+
+        // Leave scene
+        if (NetworkManager.Singleton.IsServer)
+        {
+            localPlayer.ChangeScene("Core Game");
+        }
+    }
+}
