4a0255afbbda47bd15825dda0da1ad5ad542e425
jlao3 <j.chancelao@gmail.com>
2022-04-12 11:06:44 -0700

    New new

--- Assets/Scripts/JL_UI_Manager.cs
+++ Assets/Scripts/JL_UI_Manager.cs
@@ -34,13 +34,13 @@ public class JL_UI_Manager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private void isHighlighted(int status) {
         if (status == 0) {
             this.GetComponent<Image>().sprite = asyncSprite.Result;
-            this.GetComponent<Image>().transform.localScale = new Vector2(1, 1);
+            this.GetComponent<Image>().transform.localScale = new Vector2((float)1.2, (float)1.2);
             GameObject dagger = this.gameObject.transform.GetChild(1).gameObject;
             dagger.SetActive(false);
         }
         else if (status == 1) {
             this.GetComponent<Image>().sprite = asyncSpriteSelected.Result;
-            this.GetComponent<Image>().transform.localScale = new Vector2((float)1.2, (float)1.2);
+            this.GetComponent<Image>().transform.localScale = new Vector2((float)1.4, (float)1.4);
             GameObject dagger = this.gameObject.transform.GetChild(1).gameObject;
             dagger.SetActive(true);
         }
