d280c69051ff39dba99bccab1406b83f0baea1ce
jlao3 <j.chancelao@gmail.com>
2022-04-12 17:31:03 -0700

    Fully Integrated Menu

--- Assets/Scripts/Manager Scripts/MenuManager.cs
+++ Assets/Scripts/Manager Scripts/MenuManager.cs
@@ -18,6 +18,15 @@ public class MenuManager : Singleton<MenuManager>
         menuScenes[id].SetActive(true);
     }
 
+    public void resolutionChange(int resNumber) { // Set resNumber with Button function in Unity Editor Scene
+        if (resNumber == 1920) {
+            Screen.SetResolution(1920, 1080, true); // True == Fullscreen
+        }
+        else if (resNumber == 2560) {
+            Screen.SetResolution(2560, 1440, true);
+        }
+    }
+
     public void CloseApplication()
     {
         Application.Quit();
