4ed4ff51732a4045469c5c051b49108c2b861c53
pbsulliv <pbsulliv@ucsc.edu>
2022-04-17 19:58:51 -0700

    Added All the UI Panels
    Lots and lots of panels for the Character Sheet and Inventory

--- Assets/Scripts/Ui Scripts/DragUIPanels.cs
+++ Assets/Scripts/Ui Scripts/DragUIPanels.cs
@@ -6,10 +6,11 @@ using UnityEngine.EventSystems;
 public class DragUIPanels : EventTrigger
 {
     private bool dragMe;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
