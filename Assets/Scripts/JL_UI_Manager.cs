9fc65360ba2b8e7e683f073e809cd3ff00fd8296
jlao3 <j.chancelao@gmail.com>
2022-04-20 13:25:53 -0700

    Main Menu + Pause Menu Scene
    - Fixed Button Bug that causes white box
    
    - Added Basic Profile Icon into Main Menu Scene
    
    - Added HOW TO PLAY button to Options, but doesn't do anything yet
    
    - Updated Pause Menu to be more in line
    
    - Tried to address Milo's critique of the return button being bigger than the START GAME button

--- Assets/Scripts/JL_UI_Manager.cs
+++ Assets/Scripts/JL_UI_Manager.cs
@@ -9,14 +9,9 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class JL_UI_Manager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    AsyncOperationHandle<Sprite> asyncSprite;
-    AsyncOperationHandle<Sprite> asyncSpriteSelected;
 
-    void Start() {
-        asyncSprite = Addressables.LoadAssetAsync<Sprite>("Assets/Game_Resources/UI/scroll_button.png");
-        asyncSpriteSelected = Addressables.LoadAssetAsync<Sprite>("Assets/Game_Resources/UI/scroll_button_selected.png");
-
-    }
+    public Sprite button;
+    public Sprite buttonSelected;
 
     public void OnPointerDown(PointerEventData eventData) {
         this.GetComponent<AudioSource>().Play();
@@ -34,17 +29,31 @@ public class JL_UI_Manager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     }
 
     private void isHighlighted(int status) {
-        if (status == 0) {
-            this.GetComponent<Image>().sprite = asyncSprite.Result;
-            this.GetComponent<Image>().transform.localScale = new Vector2((float)1.2, (float)1.2);
+        if (this.name == "Profile_BUTTON" || this.name == "SaveProfile_BUTTON") {
+            //spriteScale(status);
+        }
+        else {
+            if (status == 0) {
+            this.GetComponent<Image>().sprite = button;
+            spriteScale(status);
             GameObject dagger = this.gameObject.transform.GetChild(1).gameObject;
             dagger.SetActive(false);
+            }
+            else if (status == 1) {
+                this.GetComponent<Image>().sprite = buttonSelected;
+                spriteScale(status);
+                GameObject dagger = this.gameObject.transform.GetChild(1).gameObject;
+                dagger.SetActive(true);
+            }
+        }
+    }
+
+    private void spriteScale(int status) {
+        if (status == 0) {
+            this.GetComponent<Image>().transform.localScale = new Vector2((float)1.2, (float)1.2);
         }
         else if (status == 1) {
-            this.GetComponent<Image>().sprite = asyncSpriteSelected.Result;
             this.GetComponent<Image>().transform.localScale = new Vector2((float)1.4, (float)1.4);
-            GameObject dagger = this.gameObject.transform.GetChild(1).gameObject;
-            dagger.SetActive(true);
         }
     }
 }
