6a6efc842976778caa553d4a6a5a906d09947690
jlao3 <j.chancelao@gmail.com>
2022-04-22 13:16:42 -0700

    Allow you to access Credits in Main Menu

---
+++ Assets/Scripts/JLCreditsMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class JLCreditsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.anyKeyDown) {
+           SceneManager.LoadScene("JLMainMenu");
+       } 
+    }
+}
