a054b7a183047c39ef84ddaa295b24b12c020345
jlao3 <j.chancelao@gmail.com>
2022-05-12 21:57:16 -0700

    Synced Volume Sliders between Scenes

--- Assets/Scripts/JLAudioManager.cs
+++ Assets/Scripts/JLAudioManager.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class JLAudioManager : Singleton<JLAudioManager>
 {
     public JLSoundClass[] soundArray;
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider effectsSlider;
+    public Slider voiceSlider;
 
     public AudioMixer masterMixer;
     public AudioMixer musicMixer;
@@ -27,6 +31,37 @@ public class JLAudioManager : Singleton<JLAudioManager>
         }
     }
 
+    private void Start() {
+        if (masterSlider)
+        {
+            float mixerValue;
+            bool result = masterMixer.GetFloat("MasterMixer", out mixerValue);
+            if (result)
+                masterSlider.value = Mathf.Clamp(Mathf.Pow(10, mixerValue / 20), 0.0001f, 4f);
+        }
+        if (musicSlider)
+        {
+            float mixerValue;
+            bool result = musicMixer.GetFloat("MusicMasterMixer", out mixerValue);
+            if (result)
+                musicSlider.value = Mathf.Clamp(Mathf.Pow(10, mixerValue / 20), 0.0001f, 4f);
+        }
+        if (effectsSlider)
+        {
+            float mixerValue;
+            bool result = effectsMixer.GetFloat("EffectsMasterMixer", out mixerValue);
+            if (result)
+                effectsSlider.value = Mathf.Clamp(Mathf.Pow(10, mixerValue / 20), 0.0001f, 4f);
+        }
+        if (voiceSlider)
+        {
+            float mixerValue;
+            bool result = voiceMixer.GetFloat("VoiceMasterMixer", out mixerValue);
+            if (result)
+                voiceSlider.value = Mathf.Clamp(Mathf.Pow(10, mixerValue / 20), 0.0001f, 4f);
+        }
+    }
+    
     // FUNCTIONS TO USE FOR BUTTONS RATHER THAN SCRIPTS
     public void PlaySound(string soundName) {
         JLSoundClass s = Array.Find(soundArray, sound => sound.name == soundName);
