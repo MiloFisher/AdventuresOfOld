9f3a7e45d32c59a18e3cccc45bc4d164707adad5
jlao3 <j.chancelao@gmail.com>
2022-05-14 09:31:18 -0700

    Some work on Campaign Success Screen

--- Assets/Scripts/JLGameStateMenu.cs
+++ Assets/Scripts/JLGameStateMenu.cs
@@ -5,13 +5,23 @@ using TMPro;
 
 public class JLGameStateMenu : MonoBehaviour
 {
-    public GameObject[] gravestones;
-
+    public GameObject[] gravestones; // For Failure Menu
+    public GameObject[] playerSilhouettes; // For Success Menu
     public GameObject[] players;
 
     void Awake()
     {
         if (SceneManager.GetActiveScene().name == "JLFailureMenu") {
+            //int playerList = 6;
+            //gravestones[playerList - 1].SetActive(true);
+            //SetPlayerName(playerList);
+            
+            players = GameObject.FindGameObjectsWithTag("Player");
+            gravestones[players.Length - 1].SetActive(true);
+            SetPlayerName(players);
+        }
+
+        else if (SceneManager.GetActiveScene().name == "JLSuccessMenu") {
             //int playerList = 6;
             //gravestones[playerList - 1].SetActive(true);
             //SetPlayerName(playerList);
@@ -32,6 +42,17 @@ public class JLGameStateMenu : MonoBehaviour
     }*/
 
     private void SetPlayerName(GameObject[] players) {
+        GameObject graveyard = gravestones[players.Length - 1];
+        int i = 0;
+        foreach (GameObject player in players) {
+            GameObject currGrave = graveyard.transform.GetChild(i).gameObject; //Obtaining the TMP child
+            TextMeshProUGUI graveText = currGrave.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>(); //Obtaining the text component of child
+            graveText.text = player.GetComponent<Player>().Name.Value + "";
+            i++;
+        }
+    }
+
+    private void GetPlayerClass(GameObject[] players) {
         GameObject graveyard = gravestones[players.Length - 1];
         int i = 0;
         foreach (GameObject player in players) {
