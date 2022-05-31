ea107af63afab10226963b276379b528b5035029
Milo Fisher <tbfmilo@gmail.com>
2022-05-31 14:38:17 -0700

    Updated text chat
    -Added whisper feature

--- Assets/Scripts/Manager Scripts/TextChatManager.cs
+++ Assets/Scripts/Manager Scripts/TextChatManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using AdventuresOfOldMultiplayer;
 
 public class TextChatManager : Singleton<TextChatManager>
 {
@@ -86,8 +87,56 @@ public class TextChatManager : Singleton<TextChatManager>
 
     public void SendInputMessage()
     {
-        string message = "<color=" + PlayManager.Instance.GetPlayerColorString(PlayManager.Instance.localPlayer) + ">[" + PlayManager.Instance.localPlayer.Name.Value + "]</color> " + textInput.text;
+        if (IsCommand(textInput.text))
+            return;
+        string message = "<color=" + PlayManager.Instance.GetPlayerColorString(PlayManager.Instance.localPlayer) + ">[" + PlayManager.Instance.localPlayer.Name.Value + "] " + textInput.text + "</color>";
         PlayManager.Instance.localPlayer.SendMessage(message);
         textInput.text = default;
+    }
+
+    public bool IsCommand(string message)
+    {
+        if (string.IsNullOrWhiteSpace(message))
+            return false;
+        string[] words = message.Split(" ");
+        if(words.Length > 1)
+        {
+            switch(words[0])
+            {
+                case "/whisper":
+                    Player p = GetPlayerFromName(words[1]);
+                    if (p != default)
+                    {
+                        string msg = "";
+                        for (int i = 2; i < words.Length; i++)
+                            msg += " " + words[i];
+                        SendMessage("<color=#8A3986>You whispered to " + p.Name.Value + ":" + msg + "</color>");
+                        PlayManager.Instance.localPlayer.SendMessage("<color=#8A3986>" + PlayManager.Instance.localPlayer.Name.Value + " whispers:" + msg + "</color>", p.UUID.Value + "");
+                    }
+                    else
+                    {
+                        SendMessage("<color=red>[System] Whisper failed to send.  Unknown target player \"" + words[1] + "\"</color>");
+                    }
+                    textInput.text = default;
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private Player GetPlayerFromName(string name)
+    {
+        string[] nameWords = name.Split("_");
+        name = nameWords[0];
+        for (int i = 1; i < nameWords.Length; i++)
+            name += " " + nameWords[i];
+
+        for(int i = 0; i < PlayManager.Instance.playerList.Count; i++)
+        {
+            if (PlayManager.Instance.playerList[i].Name.Value == name)
+                return PlayManager.Instance.playerList[i];
+        }
+        return default;
     }
 }
